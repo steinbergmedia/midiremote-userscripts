5078af19200d1bf430a768ebc51e5ee933dcaf11
Robert Woodcock <woodcock_rob@hotmail.com>
2022-05-21 14:44:11 +1000

    Seemingly complete input surface

--- icon/platformmplus/icon_platformmplus.js
+++ icon/platformmplus/icon_platformmplus.js
@@ -6,7 +6,7 @@
 var midiremote_api = require('midiremote_api_v1')
 
 // create the device driver main object
-var deviceDriver = midiremote_api.makeDeviceDriver('Icon', 'PlatformMPlus', 'Big Fat Wombat')
+var deviceDriver = midiremote_api.makeDeviceDriver('Icon', 'Platform Mplus', 'Big Fat Wombat')
 
 // create objects representing the hardware's MIDI ports
 var midiInput = deviceDriver.mPorts.makeMidiInput()
@@ -29,6 +29,8 @@ function makeKnobStrip(knobIndex, x, y) {
 
     knobStrip.knob = surface.makeKnob(x + 2 * knobIndex, y, 1, 1)
     knobStrip.knob.mSurfaceValue.mMidiBinding.setInputPort(midiInput).bindToControlChange(0, 16 + knobIndex).setTypeRelativeSignedBit()
+    knobStrip.touched = surface.makeButton(x + 2 * knobIndex, y+1, 1, 1)
+    knobStrip.touched.mSurfaceValue.mMidiBinding.setInputPort(midiInput).bindToNote(0, 32 + knobIndex)
 
     return knobStrip
 }
@@ -94,11 +96,42 @@ function makeTransport(x, y) {
     transport.btnCycle = surface.makeButton(x+1, y+4, w, h)
     bindMidiNote(transport.btnCycle, 0, 86)
 
+    // ! The Note on/off events for the special functioans are timestamped at the same time
+    // ! cubase midi remote doesn't show anything on screen though a note is sent
+
+    // Flip - Simultaneous press of Pre Chn+Pre Bank
+    transport.btnFlip = surface.makeButton(x+3, y+4, 1, 1)
+    bindMidiNote(transport.btnFlip, 0, 50)
+
+    // Pressing the Zoom keys simultaneously will toggle on and off a note event. If on
+    // either zoom button will send a Note 100 when zoom is activated or deactivated by either button
+    // If zoom is active and you simply press then other button the event will not be sent
+    //
+    transport.btnZoomOnOff= surface.makeButton(x+4, y+4, 1, 1)
+    bindMidiNote(transport.btnZoomOnOff, 0, 100)
+
+
+    // The Jog wheel will change CC/Note based on which of thte Zoom buttons have been activated
+    // None - CC 60
+    // Vertical - Note Clockwise  97, CounterClockwise 96
+    // Horizontal - Note Clockwise  99, CounterClockwise 98
     //jog wheel
     transport.jog_wheel = surface.makeKnob(x, y + 6, 2, 2)
     transport.jog_wheel.mSurfaceValue.mMidiBinding.setInputPort(midiInput).bindToControlChange(0, 60).setTypeRelativeSignedBit()
     transport.btnJog = surface.makeButton(x,y+8,2,1)
     bindMidiNote(transport.btnJog, 0, 101)
+
+    //Zoom Vertical
+    transport.zoomVertOut = surface.makeButton(x+3, y + 6, 1, 1)
+    bindMidiNote(transport.zoomVertOut, 0, 96)
+    transport.zoomVertIn = surface.makeButton(x+4, y + 6, 1, 1)
+    bindMidiNote(transport.zoomVertIn, 0, 97)
+
+    //Zoom Horizontal
+    transport.zoomHorizOut = surface.makeButton(x+3, y + 7, 1, 1)
+    bindMidiNote(transport.zoomHorizOut, 0, 98)
+    transport.zoomHorizIn = surface.makeButton(x+4, y + 7, 1, 1)
+    bindMidiNote(transport.zoomHorizIn, 0, 99)
 
     return transport
 }
@@ -116,10 +149,11 @@ function makeSurfaceElements() {
 
     for(var i = 0; i < surfaceElements.numStrips; ++i) {
         surfaceElements.knobStrips[i] = makeKnobStrip(i, xKnobStrip, yKnobStrip)
-        surfaceElements.faderStrips[i] = makeFaderStrip(i, xKnobStrip, yKnobStrip+2)
+        surfaceElements.faderStrips[i] = makeFaderStrip(i, xKnobStrip, yKnobStrip+3)
     }
 
-    surfaceElements.transport = makeTransport(20, 3)
+    surfaceElements.faderMaster = makeFaderStrip(surfaceElements.numStrips, xKnobStrip+1, yKnobStrip+3)
+    surfaceElements.transport = makeTransport(xKnobStrip+20, yKnobStrip+3)
 
     return surfaceElements
 }
